845682b43eefade8c1e122d4458d5cfc44a30897
Guilherme Henrique <guilhermeh000@hotmail.com>
2024-12-24 00:48:16 -0300

    +++

--- src/app/core/models/i-usuario.model.ts
+++ src/app/core/models/i-usuario.model.ts
@@ -1,15 +1,15 @@
 import { ModeloEstado } from "@fynnc.models";
 
 export interface IUsuario  extends ModeloEstado<IUsuario>{
-    id: number;
-    nome: string;
-    nomeCompleto: string;
-    email: string;
-    telefone: string;
-    dataNascimento: Date;
-    sexo: 'masculino' | 'feminino' | 'outro' | 'não informado';
-    nacionalidade: string;
-    estadoCivil: 'solteiro' | 'casado' | 'divorciado' | 'viúvo' | 'outro';
+    id?: number;
+    nome?: string;
+    nomeCompleto?: string;
+    email?: string;
+    telefone?: string;
+    dataNascimento?: Date;
+    sexo?: 'masculino' | 'feminino' | 'outro' | 'não informado';
+    nacionalidade?: string;
+    estadoCivil?: 'solteiro' | 'casado' | 'divorciado' | 'viúvo' | 'outro';
     profissao?: string;
     formacaoAcademica?: string;
     username?: string;
--- src/app/core/models/usuario.model.ts
+++ src/app/core/models/usuario.model.ts
@@ -1,15 +1,20 @@
-import { ModeloEstado, IUsuario } from "@fynnc.models";
+import { ModeloEstado, IUsuario } from '@fynnc.models';
 
 export class Usuario extends ModeloEstado<IUsuario> implements IUsuario {
-  private _id: number;
-  private _nome: string;
-  private _nomeCompleto: string;
-  private _email: string;
-  private _telefone: string;
-  private _dataNascimento: Date;
-  private _sexo: 'masculino' | 'feminino' | 'outro' | 'não informado';
-  private _nacionalidade: string;
-  private _estadoCivil: 'solteiro' | 'casado' | 'divorciado' | 'viúvo' | 'outro';
+  private _id?: number;
+  private _nome?: string;
+  private _nomeCompleto?: string;
+  private _email?: string;
+  private _telefone?: string;
+  private _dataNascimento?: Date;
+  private _sexo?: 'masculino' | 'feminino' | 'outro' | 'não informado';
+  private _nacionalidade?: string;
+  private _estadoCivil?:
+    | 'solteiro'
+    | 'casado'
+    | 'divorciado'
+    | 'viúvo'
+    | 'outro';
   private _profissao?: string;
   private _formacaoAcademica?: string;
   private _username?: string;
@@ -32,42 +37,115 @@ export class Usuario extends ModeloEstado<IUsuario> implements IUsuario {
     this._senha = usuario.senha;
   }
 
-  get id(): number { return this._id; }
-  set id(value: number) { this._id = value; }
+  get id(): number | undefined {
+    return this._id;
+  }
+  set id(value: number) {
+    this._id = value;
+  }
 
-  get nome(): string { return this._nome; }
-  set nome(value: string) { this._nome = value; }
+  get nome(): string | undefined {
+    return this._nome;
+  }
+  set nome(value: string) {
+    this._nome = value;
+  }
 
-  get nomeCompleto(): string { return this._nomeCompleto; }
-  set nomeCompleto(value: string) { this._nomeCompleto = value; }
+  get nomeCompleto(): string | undefined {
+    return this._nomeCompleto;
+  }
+  set nomeCompleto(value: string) {
+    this._nomeCompleto = value;
+  }
 
-  get email(): string { return this._email; }
-  set email(value: string) { this._email = value; }
+  get email(): string | undefined  {
+    return this._email;
+  }
+  set email(value: string) {
+    this._email = value;
+  }
 
-  get telefone(): string { return this._telefone; }
-  set telefone(value: string) { this._telefone = value; }
+  get telefone(): string | undefined {
+    return this._telefone;
+  }
+  set telefone(value: string) {
+    this._telefone = value;
+  }
 
-  get dataNascimento(): Date { return this._dataNascimento; }
-  set dataNascimento(value: Date) { this._dataNascimento = value; }
+  get dataNascimento(): Date | undefined {
+    return this._dataNascimento;
+  }
+  set dataNascimento(value: Date) {
+    this._dataNascimento = value;
+  }
 
-  get sexo(): 'masculino' | 'feminino' | 'outro' | 'não informado' { return this._sexo; }
-  set sexo(value: 'masculino' | 'feminino' | 'outro' | 'não informado') { this._sexo = value; }
+  get sexo(): 'masculino' | 'feminino' | 'outro' | 'não informado' | undefined {
+    return this._sexo;
+  }
+  set sexo(value: 'masculino' | 'feminino' | 'outro' | 'não informado') {
+    this._sexo = value;
+  }
 
-  get nacionalidade(): string { return this._nacionalidade; }
-  set nacionalidade(value: string) { this._nacionalidade = value; }
+  get nacionalidade(): string | undefined {
+    return this._nacionalidade;
+  }
+  set nacionalidade(value: string) {
+    this._nacionalidade = value;
+  }
 
-  get estadoCivil(): 'solteiro' | 'casado' | 'divorciado' | 'viúvo' | 'outro' { return this._estadoCivil; }
-  set estadoCivil(value: 'solteiro' | 'casado' | 'divorciado' | 'viúvo' | 'outro') { this._estadoCivil = value; }
+  get estadoCivil(): 'solteiro' | 'casado' | 'divorciado' | 'viúvo' | 'outro'| undefined  {
+    return this._estadoCivil;
+  }
+  set estadoCivil(
+    value: 'solteiro' | 'casado' | 'divorciado' | 'viúvo' | 'outro'
+  ) {
+    this._estadoCivil = value;
+  }
 
-  get profissao(): string | undefined { return this._profissao; }
-  set profissao(value: string | undefined) { this._profissao = value; }
+  get profissao(): string | undefined {
+    return this._profissao;
+  }
+  set profissao(value: string | undefined) {
+    this._profissao = value;
+  }
 
-  get formacaoAcademica(): string | undefined { return this._formacaoAcademica; }
-  set formacaoAcademica(value: string | undefined) { this._formacaoAcademica = value; }
+  get formacaoAcademica(): string | undefined {
+    return this._formacaoAcademica;
+  }
+  set formacaoAcademica(value: string | undefined) {
+    this._formacaoAcademica = value;
+  }
 
-  get username(): string | undefined { return this._username; }
-  set username(value: string | undefined) { this._username = value; }
+  get username(): string | undefined {
+    return this._username;
+  }
+  set username(value: string | undefined) {
+    this._username = value;
+  }
 
-  get senha(): string | undefined { return this._senha; }
-  set senha(value: string | undefined) { this._senha = value; }
+  get senha(): string | undefined {
+    return this._senha;
+  }
+  set senha(value: string | undefined) {
+    this._senha = value;
+  }
+  JSON(): Partial<IUsuario> {
+    return {
+      id: this.id,
+      nome: this.nome,
+      nomeCompleto: this.nomeCompleto,
+      email: this.email,
+      telefone: this.telefone,
+      dataNascimento: this.dataNascimento,
+      sexo: this.sexo,
+      nacionalidade: this.nacionalidade,
+      estadoCivil: this.estadoCivil,
+      ...(this.profissao !== undefined && { profissao: this.profissao }),
+      ...(this.formacaoAcademica !== undefined && {
+        formacaoAcademica: this.formacaoAcademica,
+      }),
+      ...(this.username !== undefined && { username: this.username }),
+      ...(this.senha !== undefined && { senha: this.senha }),
+    };
+  }
 }
