f6f22fd9e82fe8389598afa6258d46c6e814fb8f
Guilherme Henrique <guilhermeh000@hotmail.com>
2024-12-24 15:26:16 -0300

    form user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
+import { FormlyConfigModule } from '../core/config/formly.config'
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { IUser } from '@fynnc.models';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
+  imports: [FormlyConfigModule, ReactiveFormsModule, FormlyModule, FormlyBootstrapModule,CommonModule]
 })
 export class LoginComponent {
+  form = new FormGroup({});
+  model: Partial<IUser> = {
+    email: '',
+    name: '',
+    phone: ''
+  };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      props: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true,
+      }
+    },
+    {
+      key: 'name',
+      type: 'input',
+      props: {
+        label: 'Full Name',
+        placeholder: 'Enter your full name',
+        required: true,
+      }
+    },
+    {
+      key: 'phone',
+      type: 'input',
+      props: {
+        label: 'Phone',
+        placeholder: 'Enter your phone number',
+        required: false,
+      }
+    }
+  ];
 
+  onSubmit(model: Partial<IUser>) {
+    if (this.form.valid) {
+      console.log('Form Submitted:', model);
+    } else {
+      console.warn('Form is invalid');
+    }
+  }
 }
