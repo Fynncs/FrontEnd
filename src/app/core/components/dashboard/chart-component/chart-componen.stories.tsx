2051e35dfe694789d0f54e79a2420604315cd277
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-01-02 21:16:59 -0300

    component desh board

---
+++ src/app/core/components/dashboard/chart-component/chart-componen.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { ChartComponentComponent } from './chart-component.component';
+import { moduleMetadata } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
+
+export default {
+  title: 'Components/ChartComponent',
+  component: ChartComponentComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule],  // Certifique-se de incluir qualquer módulo necessário
+    }),
+  ],
+} as Meta<ChartComponentComponent>;
+
+type Story = StoryObj<ChartComponentComponent>;
+
+export const Default: Story = {
+  args: {},
+  name: 'Gráfico Padrão',
+};
