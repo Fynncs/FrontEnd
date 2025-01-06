42bc7c8f4f72a32639a161e47c29ea7f6f858813
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-01-06 11:27:47 -0300

    +

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FinancialData, IFinancialData } from '@fynnc.models';
+import { FinancialData, IFinancialData, IUser, User } from '@fynnc.models';
 import { ChartComponentComponent } from 'app/core/components/dashboard/chart-component/chart-component.component';
 import { Bill } from 'app/core/models/bill.mode';
 import { IBill } from 'app/core/models/i-bill.model';
@@ -26,6 +26,7 @@ export class HomeComponent {
     currency: 'USD',
     lastUpdated: new Date('2024-08-12T10:30:00Z')
   } as IFinancialData);
+
   fakeBillData: Bill = new Bill({
     id: 1,
     description: "Pagamento de serviço de consultoria",
@@ -33,8 +34,42 @@ export class HomeComponent {
     dueDate: "2024-08-15",
     status: "Pendente"
   } as IBill);
-  fakePaymentStatusData: PaymentStatus = new PaymentStatus({
-    paidBills: this.fakeBillData,
-    unpaidBills: this.fakeBillData,
-  } as unknown as IPaymentStatus);
+
+  fakePaymentStatusData: PaymentStatus = new PaymentStatus();
+  userData: User = new User({
+    id: 1,
+    name: 'John',
+    fullName: 'John Doe',
+    email: 'johndoe@example.com',
+    phone: '+1 555-1234',
+    birthDate: new Date('1990-05-15'),
+    gender: 'male',
+    nationality: 'American',
+    maritalStatus: 'single',
+    profession: 'Software Engineer',
+    academicBackground: 'Bachelor in Computer Science',
+    username: 'johndoe',
+    password: 'password123',
+  } as IUser);
+
+  ngOnInit(): void {
+    if (!this.fakePaymentStatusData.paidBills) {
+      this.fakePaymentStatusData.paidBills = [];
+    }
+    if (!this.fakePaymentStatusData.unpaidBills) {
+      this.fakePaymentStatusData.unpaidBills = [];
+    }
+    if (!this.userData.paymentStatus) {
+      this.userData.paymentStatus = [];
+    }
+    if (!this.userData.financial) {
+      this.userData.financial = [];
+    }
+
+    this.fakePaymentStatusData.paidBills.push(this.fakeBillData);
+    this.fakePaymentStatusData.unpaidBills.push(this.fakeBillData);
+    this.userData.paymentStatus.push(this.fakePaymentStatusData);
+    this.userData.financial.push(this.fakeFinancialData);
+    console.log(this.userData)
+  }
 }
