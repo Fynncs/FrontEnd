0d2d8ef4d8146852b375f167cf264965e78382a8
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-01-07 15:49:21 -0300

    ?

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,5 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },  
-    { path: 'login', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
 ];
