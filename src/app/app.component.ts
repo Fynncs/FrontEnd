d44c534edb6f9e16bb5cb2aacb1906b6a77b4228
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-01-07 13:59:37 -0300

    animacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,27 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './core/components/nav-bar/nav-bar/nav-bar.component';
 import { Router, NavigationEnd } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('toggleNav', [
+      state('open', style({
+        width: '250px',
+      })),
+      state('closed', style({
+        width: '90px',
+      })),
+      transition('open <=> closed', [
+        animate('300ms ease-in-out') // Duração da animação
+      ]),
+    ])
+  ]
 })
 export class AppComponent {
   title = 'FYNNC';
