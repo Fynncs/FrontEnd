c66af03bf6dcaa02da176a3169b2f569f4f2021f
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-01-28 22:59:07 -0300

    fracasso animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './core/components/nav-bar/nav-bar/nav-bar.component';
 import { Router, NavigationEnd } from '@angular/router';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, query, group } from '@angular/animations';
 import { filter } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
@@ -13,16 +13,41 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [
+    // 🔹 Animação da navbar (mantida)
     trigger('toggleNav', [
-      state('open', style({
-        width: '220px',
-      })),
-      state('closed', style({
-        width: '90px',
-      })),
-      transition('open <=> closed', [
-        animate('300ms ease-in-out')
-      ]),
+      state('open', style({ width: '220px' })),
+      state('closed', style({ width: '90px' })),
+      transition('open <=> closed', [animate('300ms ease-in-out')])
+    ]),
+
+    trigger('routeAnimations', [
+      transition('* <=> *', [
+        query(':enter, :leave', style({ position: 'absolute', width: '100%' }), { optional: true }),
+        group([
+          query(
+            ':leave',
+            [
+              style({ opacity: 1, transform: 'translateY(0)' }),
+              animate(
+                '300ms ease-out',
+                style({ opacity: 0, transform: 'translateY(-20px)' })
+              )
+            ],
+            { optional: true }
+          ),
+          query(
+            ':enter',
+            [
+              style({ opacity: 0, transform: 'translateY(20px)' }),
+              animate(
+                '300ms ease-in',
+                style({ opacity: 1, transform: 'translateY(0)' })
+              )
+            ],
+            { optional: true }
+          )
+        ])
+      ])
     ])
   ]
 })
@@ -36,6 +61,9 @@ export class AppComponent {
   ) { }
   toggleNavbar() {
     this.navbarFechada = !this.navbarFechada;
+  }
+  prepareRoute(outlet: any): boolean {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
   ngOnInit(): void {
     this.router.events
