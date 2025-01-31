902f46a6e7bbf9307a45d2d3a0bf74a8d73f4334
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-01-31 14:02:24 -0300

    +++

--- src/app/cadastro-despesa/cadastro-despesa.component.ts
+++ src/app/cadastro-despesa/cadastro-despesa.component.ts
@@ -4,20 +4,41 @@ import { ChartComponentComponent } from 'app/core/components/dashboard/chart-com
 import { ExpenseCardComponent } from 'app/core/components/dashboard/expense-card/expense-card.component';
 import { TableComponent } from 'app/core/components/entity-creation/table/table.component';
 import { CommonModule } from '@angular/common';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @Component({
   selector: 'app-cadastro-despesa',
-  imports: [ChartComponentComponent, ExpenseCardComponent, TableComponent, CommonModule],
+  imports: [ChartComponentComponent, ExpenseCardComponent, MatListModule, MatFormFieldModule, TableComponent, FormsModule, CommonModule, MatListModule, MatMenuModule, MatIconModule],
   templateUrl: './cadastro-despesa.component.html',
   styleUrl: './cadastro-despesa.component.scss'
 })
-export class CadastroDespesaComponent{
+export class CadastroDespesaComponent {
   @ViewChild('datePickerButton') datePickerButton!: ElementRef;
   selectedDateRange: string = new Date().toDateString();
   selectedTime: string = '24h';
   showTimeOptions: boolean = false
-  constructor(private cdr: ChangeDetectorRef) {}
+  times: string[] = this.generateTimeOptions();
+  constructor(private cdr: ChangeDetectorRef) { }
+  generateTimeOptions(): string[] {
+    const times = [];
+    let hours = 0;
+    while (hours < 24) {
+      const time = this.formatTime(hours);
+      times.push(time);
+      hours += 6;
+    }
+    return times;
+  }
+  formatTime(hours: number): string {
+    const formattedHours = hours < 10 ? '0' + hours : hours;
+    return `${formattedHours}:00`;
+  }
+
   ngAfterViewInit() {
     setTimeout(() => {
       flatpickr(this.datePickerButton.nativeElement, {
@@ -33,5 +54,7 @@ export class CadastroDespesaComponent{
       });
     }, 0);
   }
-  
+  onTimeChange(event: any) {
+    console.log('Horário selecionado:', this.selectedTime);
+  }
 }
