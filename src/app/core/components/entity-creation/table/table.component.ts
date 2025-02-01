2726fd84df686efc32ff8967af1f5e383afccf3c
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-02-01 13:27:54 -0300

    +++

--- src/app/core/components/entity-creation/table/table.component.ts
+++ src/app/core/components/entity-creation/table/table.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
+  gastos = [
+    {
+      categoria: 'Alimentação',
+      dataVencimento: '15/02/2025',
+      valor: 126.00,
+      status: 'Aguardando'
+    }
+  ];
+  calcularTempoRestante(dataVencimento: string): string {
+    const hoje = new Date();
+    const vencimento = new Date(dataVencimento.split("/").reverse().join("-"));
+    const diff = vencimento.getTime() - hoje.getTime();
 
+    if (diff <= 0) {
+      return 'Vencido';
+    }
+
+    const diasRestantes = Math.floor(diff / (1000 * 60 * 60 * 24));
+    return `${diasRestantes} dias restantes`;
+  }
 }
