899a28bf4ed00032e11bcf11482965ac6e9242ff
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-02-04 17:45:54 -0300

    ++

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { ApplicationConfig, NgModule } from '@angular/core';
 import { ExpenseCardComponent } from './core/components/dashboard/expense-card/expense-card.component';
 import { CadastroDespesaComponent } from './cadastro-despesa/cadastro-despesa.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'cadastro-despesas', component: CadastroDespesaComponent,  data: { title: 'Cadastrar Despesas'  }},
     { path: 'login', component: LoginComponent },
-    { path: 'home', component:  ExpenseCardComponent},
+    { path: 'home', component:  HomeComponent},
 ];
 export const appConfig: ApplicationConfig = {
     providers: [provideRouter(routes, withDebugTracing())]
