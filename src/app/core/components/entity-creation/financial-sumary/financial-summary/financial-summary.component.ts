19c2c0dc94335097f1df8c1897e83260f0399430
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-02-05 12:16:23 -0300

    graph line

--- src/app/core/components/entity-creation/financial-sumary/financial-summary/financial-summary.component.ts
+++ src/app/core/components/entity-creation/financial-sumary/financial-summary/financial-summary.component.ts
@@ -1,91 +1,67 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { Chart, LineElement, Title, Tooltip, Legend, CategoryScale, LinearScale, PointElement } from 'chart.js';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-financial-summary',
   templateUrl: './financial-summary.component.html',
   styleUrls: ['./financial-summary.component.scss']
 })
-export class FinancialSummaryComponent implements AfterViewInit {
+export class FinancialSummaryComponent implements OnInit {
+  constructor() {}
 
-  ngAfterViewInit() {
-    console.log("Inicializando o gráfico...");
-    this.initialChart();
-  }
-  async initialChart() {
-    Chart.register(
-      LineElement,
-      Title,
-      Tooltip,
-      Legend,
-      CategoryScale,
-      LinearScale,
-      PointElement
-    );
-    const chartConfig = this.getChartConfig();
-    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
-    
-    new Chart(ctx, chartConfig);
+  Month: Date = new Date(); // Inicializa a data com a data atual
+  Months: string[] = [];
+
+  ngOnInit(): void {
+    this.generateNext12Months();
+    this.createChart();
   }
 
-  getChartConfig(): any {
-    const commonData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [{
-        label: 'Financial Data',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        borderColor: '#42A5F5',
-        backgroundColor: 'rgba(66, 165, 245, 0.2)',
-        fill: true,
-      }]
-    };
-  
-    const commonOptions = {
-      responsive: true,
-      plugins: {
-
-        tooltip: {
-          mode: 'index',
-          intersect: false
-        },
-        legend: {
-          display: false  // Desabilita a exibição da legenda
-        }
+  createChart(): void {
+    var ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    new Chart(ctx, {
+      type: 'line', 
+      data: {
+        labels: this.Months, // Corrigido aqui para usar diretamente o array `Months`
+        datasets: [{
+          label: 'Vendas',
+          data: [12, 19, 3], // Dados para o gráfico
+          borderColor: 'rgb(75, 192, 192)', // Cor da linha
+          borderWidth: 1
+        }]
       },
-      interaction: {
-        mode: 'nearest',
-        intersect: false,  // Desabilita o destaque dos pontos
-      }
-    };
-  
-    return {
-      type: 'line',
-      data: commonData,
       options: {
-        ...commonOptions,
+        plugins: {
+          legend: {
+            display: false // Isso desativa a legenda
+          }
+        },
         scales: {
-          x: {
-            ticks: {
-              color: '#000000',
-              display: false  // Remove os rótulos do eixo X (meses)
-            },
-            grid: {
-              display: false  // Remove as linhas de fundo do eixo X
-            }
-          },
           y: {
             beginAtZero: true,
-            ticks: {
-              color: '#000000',
-              display: false  // Remove os valores e a linha do eixo Y
-            },
             grid: {
-              display: false  // Remove as linhas de fundo do eixo Y
+              display: false // Isso desativa a grade (grid) de fundo no eixo Y
+            }
+          },
+          x: {
+            grid: {
+              display: false // Isso desativa a grade (grid) de fundo no eixo X
             }
           }
         }
       }
-    };
+    });
   }
   
+  
+
+  generateNext12Months(): void {
+    this.Months = Array.from({ length: 12 }, (_, i) => {
+      const nextMonth = new Date(this.Month);
+      nextMonth.setMonth(this.Month.getMonth() + i);
+      return nextMonth.toLocaleString('pt-BR', { month: 'long' });
+    });
+  }
 }
