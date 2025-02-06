548fce580f54cca4fdd2c8d760a1386e4b7aa24c
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-02-06 16:46:45 -0300

    Fix Deploy

--- src/app/core/components/dashboard/financial-summary/financial-summary.component.ts
+++ src/app/core/components/dashboard/financial-summary/financial-summary.component.ts
@@ -1,83 +1,179 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { Chart, registerables } from 'chart.js';
-
-Chart.register(...registerables);
+import { CommonModule } from '@angular/common';
+import { Component, AfterViewInit, Input } from '@angular/core';
+import { IUser } from '@fynnc.models';
+import { PaymentStatus } from 'app/core/models/payment-status.model';
+import {
+  Chart,
+  LinearScale,
+  BarElement,
+  CategoryScale,
+  Title,
+  Tooltip,
+  Legend,
+  BarController,
+  BubbleController,
+  RadarController,
+  DoughnutController,
+  ScatterController,
+  PointElement,
+  LineElement,
+  LineController,
+  ArcElement,
+  RadialLinearScale,
+  PieController
+} from 'chart.js';
 
 @Component({
   selector: 'app-financial-summary',
   templateUrl: './financial-summary.component.html',
   styleUrls: ['./financial-summary.component.scss']
 })
-export class FinancialSummaryComponent implements OnInit {
-  @ViewChild('chartCanvas', { static: true }) chartCanvas!: ElementRef<HTMLCanvasElement>;
-
-  Month: Date = new Date();
+export class FinancialSummaryComponent implements AfterViewInit  {
+  chart!: Chart;
+  // @Input() User?: IUser;
+  // @Input() Type?: 'bar' | 'radar' | 'pie' | 'line' | 'doughnut';
+  @Input() Month?: Date;
+  // @Input() paymentStatus?: PaymentStatus;
   Months: string[] = [];
+  // chartType: 'bar' | 'radar' | 'pie' | 'line' | 'doughnut' = 'bar';
+  // selectedType: string = 'bar';
 
-  constructor() {}
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.Month = this.Month ?? new Date();
     this.generateNext12Months();
-    this.createChart();
   }
 
-  createChart(): void {
-    const ctx = this.chartCanvas.nativeElement.getContext('2d');
+  ngAfterViewInit() {
+    this.initialChart();
+  }
 
-    if (!ctx) return;
+  async initialChart() {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+    Chart.register(
+      LinearScale,
+      BarElement,
+      ScatterController,
+      CategoryScale,
+      Title,
+      Tooltip,
+      Legend,
+      BarController,
+      BubbleController,
+      RadarController,
+      PointElement,
+      LineElement,
+      PieController,
+      LineController,
+      DoughnutController,
+      ArcElement,
+      RadialLinearScale
+    );
 
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: this.Months,
-        datasets: [
-          {
-            label: 'Gasto',
-            data: [6, 84, 1, 26, 1, 52, 62, 33, 82, 82, 12, 100],
-            borderColor: '#ffffff', // Corrigido
-            borderWidth: 1
-          }
-        ]
-      },
-      options: {
-        plugins: {
-          legend: {
-            display: false
+    const chartConfig = this.getChartConfig();
+    this.chart = new Chart('myChart', chartConfig);
+  }
+
+  getChartConfig(): any {
+    this.Month?.getMonth();
+    const months = this.Months;
+    let data: number[] = [];
+    let commonData: any;
+    commonData = this.createCommonData(months, data, 'Gastos Mensais');
+    const commonOptions = {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+          position: 'top',
+          labels: {
+            font: {
+              size: 14,
+              weight: 'bold'
+            },
+            color: '#ffff'
+            
           }
         },
+        tooltip: {
+          callbacks: {
+            label: (context: any) => {
+              return `R$ ${context.raw.toLocaleString('pt-BR')}`;
+            }
+          },
+          backgroundColor: '#19202b',
+          titleColor: '#A0C1D1',
+          bodyColor: '#D4EAF7'
+        }
+      },
+      animation: {
+        duration: 1000,
+        easing: 'easeInOutQuad'
+      },
+      onClick: (event: any, elements: any[]) => {
+        if (commonData.datasets[0].onClick) {
+          commonData.datasets[0].onClick(event, elements);
+        }
+      }
+    };
+
+    return {
+      type: 'line',
+      data: commonData,
+      options: {
+        
+        ...commonOptions,
         scales: {
+          x: {
+            ticks: {
+              font: {
+                size: 13, 
+              },
+              
+              color: '#ffff'
+            },
+            grid: {
+              display: false,
+              borderColor: 'transparent'
+            },
+          },
           y: {
             beginAtZero: true,
             display: false,
             grid: {
-              display: false
+              display: false 
             },
             ticks: {
-              stepSize: 1,
-              callback: function (value) {
+              stepSize: 1, 
+              callback: function(value: any) {
                 return Number(value).toFixed(0);
               }
             }
           },
-          x: {
-            display: true, // 'auto' não é válido
-            grid: {
-              display: false
-            },
-            ticks: {
-              color: '#ffffff' // Corrigido
-            }
-          }
         }
       }
-    });
+    };
   }
 
-  generateNext12Months(): void {
+  createCommonData(labels: string[], data: number[], label: string): object {
+    return {
+      labels: labels,
+      datasets: [{
+        label: label,
+        data: [6, 84, 1, 26, 1, 52, 62, 33, 82, 82, 12, 100],
+        backgroundColor: "#ffff",
+        borderColor: '#ffff',
+        borderWidth: 1.0,
+        hoverBorderWidth: 2,
+      }]
+    };
+  }
+  generateNext12Months() {
     this.Months = Array.from({ length: 12 }, (_, i) => {
-      const nextMonth = new Date(this.Month);
-      nextMonth.setMonth(this.Month.getMonth() + i);
-      return nextMonth.toLocaleString('pt-BR', { month: 'long' }).replace(/^./, (str) => str.toUpperCase());
+      const nextMonth = new Date(this.Month as Date);
+      if (this.Month) nextMonth.setMonth(this.Month.getMonth() + i);
+      return nextMonth.toLocaleString('pt-BR', { month: 'long' });
     });
   }
 }
