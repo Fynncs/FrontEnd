d932df83f3264f7295214d43a1597c6a453ad1aa
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-02-06 15:33:55 -0300

    +

--- src/app/core/components/dashboard/financial-summary/financial-summary.component.ts
+++ src/app/core/components/dashboard/financial-summary/financial-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
@@ -8,74 +8,51 @@ Chart.register(...registerables);
   templateUrl: './financial-summary.component.html',
   styleUrls: ['./financial-summary.component.scss']
 })
-export class FinancialSummaryComponent implements OnInit {
+export class FinancialSummaryComponent implements AfterViewInit  {
+  @ViewChild('myChart', { static: false }) chartRef!: ElementRef<HTMLCanvasElement>;
   constructor() {}
 
-  Month: Date = new Date(); // Inicializa a data com a data atual
-  Months: string[] = [];
+  Months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-  ngOnInit(): void {
-    this.generateNext12Months();
-    this.createChart();
-  }
-
-  createChart(): void {
-    
-    var ctx = document.getElementById('myChart') as HTMLCanvasElement;
+  ngAfterViewInit() {
+    const ctx = this.chartRef.nativeElement;
     ctx.height = 100;
     ctx.width = 500;
+
     new Chart(ctx, {
-      type: 'line', 
+      type: 'line',
       data: {
-        labels: this.Months, 
-        datasets: [{
-          label: 'Gasto',
-          data: [6, 84, 1, 26, 1, 52, 62, 33, 82, 82, 12, 100. ], 
-          borderColor: ['#ffff'], 
-          borderWidth: 1
-        }]
+        labels: this.Months,
+        datasets: [
+          {
+            label: 'Gasto',
+            data: [6, 84, 1, 26, 1, 52, 62, 33, 82, 82, 12, 100],
+            borderColor: ['#ffff'],
+            borderWidth: 1,
+          },
+        ],
       },
       options: {
         plugins: {
-          legend: {
-            display: false 
-          }
+          legend: { display: false },
         },
         scales: {
           y: {
             beginAtZero: true,
             display: false,
-            grid: {
-              display: false 
-            },
+            grid: { display: false },
             ticks: {
-              stepSize: 1, 
-              callback: function(value) {
-                return Number(value).toFixed(0);
-              }
-            }
+              stepSize: 1,
+              callback: (value) => Number(value).toFixed(0),
+            },
           },
           x: {
             display: 'auto',
-            grid: {
-              display: false 
-            },
-            ticks: {
-              color: '#ffff' // Cor das labels no eixo X
-            }
-          }
-        }
-      }
-    });
-  }
-  
-  
-
-  generateNext12Months(): void {
-    this.Months = Array.from({ length: 12 }, (_, i) => {
-      const nextMonth = new Date(this.Month);
-      nextMonth.setMonth(this.Month.getMonth() + i);
-      return nextMonth.toLocaleString('pt-BR', { month: 'long' });
+            grid: { display: false },
+            ticks: { color: '#ffff' },
+          },
+        },
+      },
     });
   }
 }
